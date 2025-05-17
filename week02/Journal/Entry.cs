12fc65e0d0bb865c46e7a9ea4169ee57960f493d
owenmeredith12 <owen.meredith1516@gmail.com>
2025-05-17 09:48:25 -0600

    final touches on week 2 project

--- week02/Journal/Entry.cs
+++ week02/Journal/Entry.cs
@@ -5,10 +5,17 @@ public class Entry
     public string _entry = " ";
     public string _date = DateTime.Now.ToShortDateString();
 
+    public int _length = 0;
+
     public string getNewEntry(string prompt)
     {
         _entry = prompt + $" {_date}\n";
-        _entry += Console.ReadLine();
+        string entry = Console.ReadLine();
+
+        string[] length = entry.Split(new char[] { ' ', '\t', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+        _length = length.Length;
+        _entry += entry;
+        _entry += $"\n{_length} words";
         return _entry;
     }
 
--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,3 +1,8 @@
+//the extra part of my program keeps track of the words in and entry by splitting
+//the entry string up by spaces or new line characters and then counting the length
+//of that string. this is then displayed at the bottom of the entry
+
+
 using System;
 
 class Program
